70ab7a0ec5c1a67b52fa80b2c70c7e16a98e1ee3
Marwan Saad <marwan10salem@hotmail.com>
2021-04-27 05:48:38 +0200

    django-heroku

--- videoservice/videoservice/settings.py
+++ videoservice/videoservice/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 
@@ -136,3 +137,4 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+django_heroku.settings(locals())
