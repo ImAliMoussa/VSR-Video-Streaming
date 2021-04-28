790489bb05cfcfeffbf57b0e59d040b2a3752e5c
Ali Moussa <imalimoussa@gmail.com>
2021-04-28 02:54:42 +0200

    created a dir for remote django backend

--- remote-backend/videoservice/video/models.py
+++ remote-backend/videoservice/video/models.py
@@ -4,10 +4,10 @@ from datetime import datetime
 # Create your models here.
 class Video(models.Model):
     title = models.CharField(max_length=1000)
-    url = models.URLField()
+    video_key_s3 = models.CharField(max_length=128)
     upload_date = models.DateTimeField(default=datetime.now)
-    thumbnail_url = models.URLField()
-    audio_url = models.URLField()
+    thumbnail_url = models.CharField(max_length=128)
+    audio_url = models.CharField(max_length=128)
     def __str__(self):
         return self.url
 
