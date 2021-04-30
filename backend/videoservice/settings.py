acc3d110b522f3b136df1f1309d21952e2217022
Ali Moussa <imalimoussa@gmail.com>
2021-04-30 20:06:40 +0200

    remove unused statement

--- backend/videoservice/settings.py
+++ backend/videoservice/settings.py
@@ -78,14 +78,11 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'videoservice.wsgi.application'
+envWSGI_APPLICATION = 'videoservice.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-database_conf = {
-    'user': env('DATABASE_NAME')
-}
 
 DATABASES = {
     'default': {
